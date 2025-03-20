bcae6f22936c3e93054d844865a9c7be51b4e9f0
Boyang Sa <bobsa514@gmail.com>
2025-03-19 23:42:36 -0700

    minor updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/geospatial-viewer/',
+  base: '/mapviewer-gl/',
   build: {
     sourcemap: true,
     chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1000kb
